d9ed68f644d9c254dedb2ac8ab94fd6eeba55dc8
jeevan1960 <52868222+jeevan1960@users.noreply.github.com>
2019-07-14 02:45:36 +0530

    Add files via upload

---
+++ JenkinsDemo/src/demoJenkins/DemoJenkinsnJobs.java
@@ -0,0 +1,20 @@
+package demoJenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class DemoJenkinsnJobs {
+	
+	@Test
+	public void testJenkins() {
+		System.out.println("Welcome to Jenkins world");
+		System.setProperty("webdriver.chrome.driver", "C:\\STS_BC\\JenkinsDemo\\lib\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
+		driver.quit();
+	}
+
+}
